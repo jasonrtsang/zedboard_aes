101cd3e671b73f3312aceb967d77f7ad4e2b6141
jrtsang <jrtsang@sfu.ca>
2018-03-14 16:02:04 -0700

    Integrate key switches as encryption key

--- sdk/main/src/main.c
+++ sdk/main/src/main.c
@@ -42,6 +42,11 @@ bool read_from_file(const char *sdFile, uint8_t *readBuf, uint32_t *readSize);
 #define TESTBIN_SIZE_64 64
 static FIL fil; // Specified file input
 
+#ifdef MILESTONE5
+#define DECRYPTION 0
+#define ENCRYPTION 1
+#endif // MILESTONE5
+
 /*****************************************************************************
 *
 * Main function
@@ -56,45 +61,39 @@ static FIL fil; // Specified file input
 int main(void)
 {
 
-    /* Fixed key and initialization vector (cbc) */
-    const uint8_t key[] = {
+    /* Fixed keys and initialization vector (cbc) */
+    const uint8_t keys[] = {
     		0x72, 0x42, 0xf8, 0xeb, 0xe2, 0xca, 0x6c, 0x20, 0x6c, 0xd8, 0xdf, 0x1a, 0xcd, 0xe3, 0xfd, 0xe7,
     		0x89, 0xb3, 0x6e, 0xae, 0x31, 0xa4, 0x73, 0x7a, 0xda, 0x5c, 0x4a, 0x41, 0x63, 0x33, 0x11, 0xbd,
     		0xf3, 0x52, 0x2a, 0x20, 0x1a, 0xfd, 0x97, 0x3e, 0x13, 0x7a, 0xec, 0x26, 0x3f, 0x8d, 0xeb, 0xdf,
     		0x18, 0x6e, 0xb8, 0x08, 0x46, 0x4a, 0xd0, 0x15, 0xbf, 0x81, 0xb7, 0xa2, 0x3b, 0x78, 0xc2, 0x44,
     		0xdc, 0x4c, 0x81, 0x75, 0x22, 0x74, 0x07, 0xae, 0x75, 0x8c, 0x65, 0xd0, 0x89, 0x25, 0x72, 0x67,
     		0xa3, 0x02, 0x2d, 0xdd, 0xdf, 0x45, 0x0d, 0x3c, 0xa3, 0x44, 0x49, 0x4a, 0xe4, 0x09, 0x15, 0x2c,
-
     		0x36, 0xe4, 0xb2, 0x70, 0xcd, 0x70, 0xc2, 0x06, 0x8c, 0xf2, 0xf9, 0x95, 0x83, 0x0e, 0x92, 0xb3,
     		0x4d, 0xe1, 0xab, 0x84, 0x86, 0x2e, 0x41, 0x1a, 0x31, 0x83, 0xf6, 0xbe, 0x6e, 0x5d, 0x25, 0xa6,
     		0x4d, 0x43, 0xf6, 0x9b, 0x72, 0xe7, 0xb0, 0xeb, 0x7c, 0xd4, 0xbb, 0x5a, 0xa3, 0xd1, 0x3e, 0xac,
     		0xa9, 0x30, 0x9b, 0x17, 0x1b, 0x53, 0xf6, 0x78, 0x41, 0xf8, 0xef, 0x79, 0x78, 0xe5, 0x5b, 0x0b,
     		0x06, 0x79, 0x72, 0x9a, 0x0d, 0xa1, 0x45, 0xa1, 0xda, 0x79, 0x17, 0x33, 0x78, 0x33, 0xbf, 0x84,
     		0xfe, 0xa8, 0x1a, 0x4b, 0x7c, 0xb0, 0x79, 0xa0, 0x64, 0x0f, 0xf3, 0x3d, 0xe6, 0x72, 0xf3, 0x1d,
-
     		0x89, 0x30, 0xa1, 0x0e, 0x41, 0xb6, 0xd8, 0x6d, 0x95, 0xfd, 0xd7, 0x79, 0xf9, 0xfb, 0xc9, 0x39,
     		0xd1, 0x9f, 0x1c, 0x99, 0x88, 0x3a, 0x1a, 0xcf, 0xc1, 0xbf, 0x93, 0x01, 0x26, 0xe2, 0x40, 0x17,
     		0xc9, 0x10, 0x64, 0x5d, 0x1c, 0xc8, 0x39, 0xec, 0x9d, 0x9e, 0x1e, 0xff, 0x4a, 0xe1, 0xe8, 0xc7,
     		0x16, 0xbe, 0xc2, 0xc3, 0x77, 0x15, 0x36, 0xc2, 0x8b, 0x29, 0xea, 0x73, 0x5b, 0x58, 0x25, 0x20,
     		0xd7, 0xac, 0x69, 0x1d, 0xbb, 0x86, 0xb3, 0x47, 0xf8, 0xf1, 0x58, 0x12, 0xd1, 0x57, 0xec, 0x83,
     		0xbc, 0x8a, 0x7e, 0xe9, 0x69, 0xf6, 0x2c, 0x86, 0x13, 0x0e, 0x13, 0x1c, 0xf4, 0x0d, 0xe6, 0x08,
-
     		0xdd, 0xc9, 0xf7, 0xc0, 0x3c, 0xab, 0x0e, 0x11, 0x7c, 0x57, 0x8e, 0x36, 0x4c, 0x57, 0xbc, 0x1f,
     		0x7c, 0xfe, 0x0a, 0x61, 0x57, 0x53, 0x83, 0x88, 0x7c, 0x70, 0x71, 0xe8, 0x5b, 0x5b, 0x0a, 0x67,
     		0xfb, 0x47, 0xcf, 0x9a, 0x7d, 0x0e, 0x9c, 0x94, 0x52, 0x54, 0xaa, 0x3a, 0xf6, 0x82, 0xd2, 0xa8,
     		0x44, 0xfa, 0x2d, 0x2d, 0x14, 0x56, 0x06, 0xcf, 0x21, 0x47, 0x75, 0xb2, 0x7c, 0x0a, 0xd7, 0x2c,
     		0x96, 0xe4, 0x38, 0xec, 0xba, 0xcc, 0x69, 0x79, 0x80, 0xc3, 0x52, 0xab, 0x5c, 0x9e, 0x0f, 0x09,
     		0xd2, 0x50, 0x4f, 0xe5, 0x86, 0xf1, 0xe9, 0xec, 0x39, 0x4b, 0x61, 0x93, 0xb6, 0x6e, 0x62, 0xc0,
-
     		0x09, 0x49, 0x49, 0x0b, 0x53, 0xfb, 0x11, 0xb2, 0xe7, 0xf9, 0x0b, 0xe5, 0xff, 0xfb, 0x9c, 0x28,
     		0x2f, 0xef, 0x79, 0x24, 0x38, 0x0f, 0xc7, 0x75, 0x92, 0x01, 0xf2, 0xa8, 0x14, 0xab, 0x4b, 0xd4,
     		0xc2, 0x38, 0xed, 0xfa, 0x29, 0x27, 0xb9, 0xa5, 0x27, 0x3d, 0x14, 0x1a, 0xe5, 0xb7, 0x46, 0x84,
     		0xf7, 0x39, 0x04, 0x23, 0x93, 0x19, 0x17, 0x47, 0x28, 0xea, 0x4c, 0x06, 0x35, 0xaf, 0x91, 0x0b,
     		0xc7, 0x65, 0x89, 0xf1, 0xf0, 0x87, 0xc7, 0xfe, 0x71, 0x2f, 0x75, 0x99, 0xcc, 0x9f, 0x9b, 0x4a,
     		0xd1, 0x2c, 0x1c, 0x5a, 0x80, 0x6c, 0x8c, 0x53, 0x84, 0xf2, 0x88, 0x99, 0x8b, 0xf5, 0x24, 0x8c,
-
     		0xeb, 0xf3, 0x03, 0xe5, 0x61, 0x25, 0x34, 0xbd, 0xe3, 0x29, 0x27, 0x26, 0x9f, 0x41, 0x96, 0x74,
-
     };
     const uint8_t iv[]  = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
 
@@ -121,17 +120,14 @@ int main(void)
 #ifdef MILESTONE5
     XGpio gpioSwitches;
     XGpio gpioLeds;
-
+    uint8_t switchKey[16];
 	uint8_t dipValue;
 
-	// Lookup the config information and store it in the struct "GPIO_Config"
-	//	gpioSwLed_Config = XGpio_LookupConfig(XPAR_SW_LED_GPIO_AXI_DEVICE_ID);
-
-	if(XGpio_Initialize(&gpioSwitches, XPAR_SW_LED_GPIO_AXI_DEVICE_ID) != XST_SUCCESS) { // AXI GPIO
-	        printf("UH OH: GPIO SW0 initialization failed\r\n");
+	if(XGpio_Initialize(&gpioSwitches, XPAR_SW_LED_GPIO_AXI_DEVICE_ID) != XST_SUCCESS) {
+		printf("UH OH: GPIO SWS initialization failed\r\n");
 	};
-	if(XGpio_Initialize(&gpioLeds, XPAR_SW_LED_GPIO_AXI_DEVICE_ID) != XST_SUCCESS) { // AXI GPIO
-	        printf("UH OH: GPIO SW0 initialization failed\r\n");
+	if(XGpio_Initialize(&gpioLeds, XPAR_SW_LED_GPIO_AXI_DEVICE_ID) != XST_SUCCESS) {
+		printf("UH OH: GPIO2 LEDS initialization failed\r\n");
 	};
 
 	// Set the direction of the bits in the GPIO.
@@ -139,40 +135,10 @@ int main(void)
 	// The upper (MSB) 8 bits of the GPIO are for the LEDs (outputs).
 	XGpio_SetDataDirection(&gpioSwitches, 1, 0x00FF);
 	XGpio_SetDataDirection(&gpioLeds, 2, 0x0000);
-
-    uint8_t switchKey[16];
 #endif // MILESTONE5
-
-
 
     /* Main application */
     while(!exitFlag) {
-
-
-    	// Go around in a loop for ever
-		while (1)
-		{
-			// Read from the GPIO to determine the position of the DIP switches
-			dipValue = XGpio_DiscreteRead(&gpioSwitches, 1);
-			// Write the value back to the LEDS
-			XGpio_DiscreteWrite(&gpioLeds, 2, dipValue);
-
-			for (i = 0; i < 16; i++) {
-				switchKey[i] = key[dipValue+i*16];
-			};
-
-			for (i = 0; i < 16; i++) {
-				switchKey[i] = key[dipValue+i*16];
-				printf("%02X", switchKey[i]);
-			};
-
-
-			printf("\r\n");
-
-		}
-
-
-
         printf("\r\n\r\n");
         printf("~~~~~~~~~~~~~~~~ AES Encryption/ Decryption ~~~~~~~~~~~~~~~\r\n");
         printf(" SELECT the operation from the below menu \r\n");
@@ -281,7 +247,7 @@ int main(void)
                         printf("Starting CBC encrypt...\r\n");
                         printf("> Name encrypted file output\r\n");
                         prompt_file_input(fileNameOut);
-                        AES_init_ctx_iv(&ctx, key, iv);
+                        AES_init_ctx_iv(&ctx, switchKey, iv);
                         AES_CBC_encrypt_buffer(&ctx, inputBuf, fileSizeRead, 0);
                         printf("Writing encrypted file to SD card...\r\n");
                         /* Create output file */
@@ -292,7 +258,7 @@ int main(void)
                         printf("Starting CBC decrypt...\r\n");
                         printf("> Name decrypted file output\r\n");
                         prompt_file_input(fileNameOut);
-                        AES_init_ctx_iv(&ctx, key, iv);
+                        AES_init_ctx_iv(&ctx, switchKey, iv);
                         AES_CBC_decrypt_buffer(&ctx, inputBuf, fileSizeRead, 0);
                         printf("Writing decrypted file to SD card...\r\n");
                         /* Create output file */
@@ -327,57 +293,19 @@ int main(void)
                         printf("Starting ECB encrypt...\r\n");
                         printf("> Name encrypted file output\r\n");
                         prompt_file_input(fileNameOut);
-
-
 #ifdef MILESTONE5
-
-
-
+                        printf("Enter encryption key via switches, press enter to continue...\r\n");
+						inbyte();
+						fflush(stdin);
+						// Read from the GPIO to determine the position of the DIP switches
+						dipValue = XGpio_DiscreteRead(&gpioSwitches, 1);
+						// Write the value back to the LEDS
+						XGpio_DiscreteWrite(&gpioLeds, 2, dipValue);
+						for (i = 0; i < 16; i++) {
+							switchKey[i] = keys[dipValue+i*16];
+                        }
 #endif // MILESTONE5
-
-                        AES_init_ctx(&ctx, key);
-
-#ifdef MILESTONE2
-                        uint32_t *baseaddr_p = (uint32_t *)XPAR_KEY_EXPANSION_0_S00_AXI_BASEADDR;
-                        /* Input key */
-                        uint32_t input_0 = key[3] | key[2] << 8 | key[1] << 16 | key[0] << 24;
-                        *(baseaddr_p+0) = input_0;
-                        uint32_t input_1 = key[7] | key[6] << 8 | key[5] << 16 | key[4] << 24;
-                        *(baseaddr_p+1) = input_1;
-                        uint32_t input_2 = key[11] | key[10] << 8 | key[9] << 16 | key[8] << 24;
-                        *(baseaddr_p+2) = input_2;
-                        uint32_t input_3 = key[15] | key[14] << 8 | key[13] << 16 | key[12] << 24;
-                        *(baseaddr_p+3) = input_3;
-                        *(baseaddr_p+48) = ENCRYPTION;
-
-                        /* Output round keys */
-                        j = 0;
-                        for (i = 4; i < 48; i++) {
-                            ctx_block.RoundKey[j++] = (*(baseaddr_p+i) >> 24) & 0xFF;
-                            ctx_block.RoundKey[j++] = (*(baseaddr_p+i) >> 16) & 0xFF;
-                            ctx_block.RoundKey[j++] = (*(baseaddr_p+i) >> 8) & 0xFF;
-                            ctx_block.RoundKey[j++] = *(baseaddr_p+i) & 0xFF;
-                        }
-
-                        /* Print IP block vs SW implementation */
-                        printf("\nHW Round Keys\n");
-                        for (i = 0; i < 176; i++) {
-                            printf("%.2x ", ctx_block.RoundKey[i]);
-                        }
-                        printf("\n\nSW Round Keys\n");
-                        for (i = 0; i < 176; i++) {
-                            printf("%.2x ", ctx.RoundKey[i]);
-                        }
-                        printf("\n");
-
-                        /* Diff SW and HW to validate */
-                        for (i = 0; i < 176; i++) {
-                            if (ctx.RoundKey[i] != ctx_block.RoundKey[i]) {
-                                printf("UH OH: SW Key Expansion does not match that of HW\r\n");
-                            }
-                        }
-#endif // MILESTONE2
-
+                        AES_init_ctx(&ctx, switchKey);
                         AES_ECB_encrypt_buffer(&ctx, inputBuf, fileSizeRead, 0);
                         printf("Writing encrypted file to SD card...\r\n");
                         /* Create output file */
@@ -388,49 +316,19 @@ int main(void)
                         printf("Starting ECB decrypt...\r\n");
                         printf("> Name decrypted file output\r\n");
                         prompt_file_input(fileNameOut);
-                        AES_init_ctx(&ctx, key);
-
-#ifdef MILESTONE3
-                        uint32_t *baseaddr_p = (uint32_t *)XPAR_KEY_EXPANSION_0_S00_AXI_BASEADDR;
-                        /* Input key */
-                        uint32_t input_0 = key[3] | key[2] << 8 | key[1] << 16 | key[0] << 24;
-                        *(baseaddr_p+0) = input_0;
-                        uint32_t input_1 = key[7] | key[6] << 8 | key[5] << 16 | key[4] << 24;
-                        *(baseaddr_p+1) = input_1;
-                        uint32_t input_2 = key[11] | key[10] << 8 | key[9] << 16 | key[8] << 24;
-                        *(baseaddr_p+2) = input_2;
-                        uint32_t input_3 = key[15] | key[14] << 8 | key[13] << 16 | key[12] << 24;
-                        *(baseaddr_p+3) = input_3;
-                        *(baseaddr_p+48) = ENCRYPTION;
-
-                        /* Output round keys */
-                        j = 0;
-                        for (i = 4; i < 48; i++) {
-                            ctx_block.RoundKey[j++] = (*(baseaddr_p+i) >> 24) & 0xFF;
-                            ctx_block.RoundKey[j++] = (*(baseaddr_p+i) >> 16) & 0xFF;
-                            ctx_block.RoundKey[j++] = (*(baseaddr_p+i) >> 8) & 0xFF;
-                            ctx_block.RoundKey[j++] = *(baseaddr_p+i) & 0xFF;
+#ifdef MILESTONE5
+                        printf("Enter encryption key via switches, press enter to continue...\r\n");
+						inbyte();
+						fflush(stdin);
+						// Read from the GPIO to determine the position of the DIP switches
+						dipValue = XGpio_DiscreteRead(&gpioSwitches, 1);
+						// Write the value back to the LEDS
+						XGpio_DiscreteWrite(&gpioLeds, 2, dipValue);
+						for (i = 0; i < 16; i++) {
+							switchKey[i] = keys[dipValue+i*16];
                         }
-
-                        /* Print IP block vs SW implementation */
-                        printf("\nHW Round Keys\n");
-                        for (i = 0; i < 176; i++) {
-                            printf("%.2x ", ctx_block.RoundKey[i]);
-                        }
-                        printf("\n\nSW Round Keys\n");
-                        for (i = 0; i < 176; i++) {
-                            printf("%.2x ", ctx.RoundKey[i]);
-                        }
-                        printf("\n");
-
-                        /* Diff SW and HW to validate */
-                        for (i = 0; i < 176; i++) {
-                            if (ctx.RoundKey[i] != ctx_block.RoundKey[i]) {
-                                printf("UH OH: SW Key Expansion does not match that of HW\r\n");
-                            }
-                        }
-#endif // MILESTONE3
-
+#endif // MILESTONE5
+                        AES_init_ctx(&ctx, switchKey);
                         AES_ECB_decrypt_buffer(&ctx, inputBuf, fileSizeRead, 0);
                         printf("Writing decrypted file to SD card...\r\n");
                         /* Create output file */
