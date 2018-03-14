df9292cef3ca682e91ea3482e6d7f0c1c69c57ef
jrtsang <jrtsang@sfu.ca>
2018-03-14 12:09:51 -0700

    Strip old milestone and verbose switch from main

--- sdk/main/src/main.c
+++ sdk/main/src/main.c
@@ -60,19 +60,6 @@ static FIL fil; // Specified file input
 ******************************************************************************/
 int main(void)
 {
-//	/* Old test strings with input and expected output for encryption */
-//	/* ECB */
-//	uint8_t in[]  = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
-//	uint8_t out[] = { 0x3a, 0xd7, 0x7b, 0xb4, 0x0d, 0x7a, 0x36, 0x60, 0xa8, 0x9e, 0xca, 0xf3, 0x24, 0x66, 0xef, 0x97 };
-//	/* CBC */
-//	uint8_t in[]  = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
-//	                      0xae, 0x2d, 0x8a, 0x57, 0x1e, 0x03, 0xac, 0x9c, 0x9e, 0xb7, 0x6f, 0xac, 0x45, 0xaf, 0x8e, 0x51,
-//	                      0x30, 0xc8, 0x1c, 0x46, 0xa3, 0x5c, 0xe4, 0x11, 0xe5, 0xfb, 0xc1, 0x19, 0x1a, 0x0a, 0x52, 0xef,
-//	                      0xf6, 0x9f, 0x24, 0x45, 0xdf, 0x4f, 0x9b, 0x17, 0xad, 0x2b, 0x41, 0x7b, 0xe6, 0x6c, 0x37, 0x10 };
-//	uint8_t out[] = { 0x76, 0x49, 0xab, 0xac, 0x81, 0x19, 0xb2, 0x46, 0xce, 0xe9, 0x8e, 0x9b, 0x12, 0xe9, 0x19, 0x7d,
-//	                      0x50, 0x86, 0xcb, 0x9b, 0x50, 0x72, 0x19, 0xee, 0x95, 0xdb, 0x11, 0x3a, 0x91, 0x76, 0x78, 0xb2,
-//	                      0x73, 0xbe, 0xd6, 0xb8, 0xe3, 0xc1, 0x74, 0x3b, 0x71, 0x16, 0xe6, 0x9e, 0x22, 0x22, 0x95, 0x16,
-//	                      0x3f, 0xf1, 0xca, 0xa1, 0x68, 0x1f, 0xac, 0x09, 0x12, 0x0e, 0xca, 0x30, 0x75, 0x86, 0xe1, 0xa7 };
 
     /* Fixed key and initialization vector (cbc) */
     const uint8_t key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
@@ -91,24 +78,12 @@ int main(void)
     static uint8_t inputBuf[10*1024*1024] __attribute__ ((aligned(32))); // 10mb buffers [1024*1024 == 1mb, 1024 == 1kb]
     static uint8_t outputBuf[10*1024*1024] __attribute__ ((aligned(32)));
 
-    static XGpio GpioSW0_Ptr;  // XPAR_AXI_GPIO_0_DEVICE_ID, SW0
-    uint32_t readSW0 = 0;
-
-#if defined(MILESTONE2) || defined(MILESTONE3)
-    int j;
-    struct AES_ctx ctx_block; // Context
-#endif // MILESTONE2 || MILESTONE3
-
     struct AES_ctx ctx; // Context
 
     bool exitFlag = false; // Application closure
 
     /* Initialize platform */
     init_platform();
-    if(XGpio_Initialize(&GpioSW0_Ptr, XPAR_SW0_AXI_GPIO_DEVICE_ID) != XST_SUCCESS) { // AXI GPIO
-        printf("UH OH: GPIO SW0 initialization failed\r\n");
-    }
-    XGpio_SetDataDirection(&GpioSW0_Ptr, 1, 1); // Set GPIO direction
 
     /* Main application */
     while(!exitFlag) {
@@ -215,37 +190,24 @@ int main(void)
                     break;
                 }
 
-                /* Check if verbose SW0 switch is on */
-                readSW0 = XGpio_DiscreteRead(&GpioSW0_Ptr, 1);
-
                 switch(modeChoice) {
                     case '1': /* Encrypt */
-                        readSW0 = XGpio_DiscreteRead(&GpioSW0_Ptr, 1);
-                        if (readSW0) {
-                            printf("Starting CBC encrypt VERBOSE...\r\n");
-                        } else {
-                            printf("Starting CBC encrypt...\r\n");
-                        }
+                        printf("Starting CBC encrypt...\r\n");
                         printf("> Name encrypted file output\r\n");
                         prompt_file_input(fileNameOut);
                         AES_init_ctx_iv(&ctx, key, iv);
-                        AES_CBC_encrypt_buffer(&ctx, inputBuf, fileSizeRead, readSW0);
+                        AES_CBC_encrypt_buffer(&ctx, inputBuf, fileSizeRead, 0);
                         printf("Writing encrypted file to SD card...\r\n");
                         /* Create output file */
                         write_to_file(fileNameOut, inputBuf, fileSizeRead);
                         printf("Done!\r\n");
                         break;
                     case '2': /* Decrypt */
-                        readSW0 = XGpio_DiscreteRead(&GpioSW0_Ptr, 1);
-                        if (readSW0) {
-                            printf("Starting CBC decrypt VERBOSE...\r\n");
-                        } else {
-                            printf("Starting CBC decrypt...\r\n");
-                        }
+                        printf("Starting CBC decrypt...\r\n");
                         printf("> Name decrypted file output\r\n");
                         prompt_file_input(fileNameOut);
                         AES_init_ctx_iv(&ctx, key, iv);
-                        AES_CBC_decrypt_buffer(&ctx, inputBuf, fileSizeRead, readSW0);
+                        AES_CBC_decrypt_buffer(&ctx, inputBuf, fileSizeRead, 0);
                         printf("Writing decrypted file to SD card...\r\n");
                         /* Create output file */
                         write_to_file(fileNameOut, inputBuf, fileSizeRead);
@@ -274,17 +236,9 @@ int main(void)
                     break;
                 }
 
-                /* Check if verbose SW0 switch is on */
-                readSW0 = XGpio_DiscreteRead(&GpioSW0_Ptr, 1);
-
                 switch(modeChoice) {
                     case '1': /* Encrypt */
-                        readSW0 = XGpio_DiscreteRead(&GpioSW0_Ptr, 1);
-                        if (readSW0) {
-                            printf("Starting ECB encrypt VERBOSE...\r\n");
-                        } else {
-                            printf("Starting ECB encrypt...\r\n");
-                        }
+                        printf("Starting ECB encrypt...\r\n");
                         printf("> Name encrypted file output\r\n");
                         prompt_file_input(fileNameOut);
                         AES_init_ctx(&ctx, key);
@@ -330,19 +284,14 @@ int main(void)
 						}
 #endif // MILESTONE2
 
-                        AES_ECB_encrypt_buffer(&ctx, inputBuf, fileSizeRead, readSW0);
+                        AES_ECB_encrypt_buffer(&ctx, inputBuf, fileSizeRead, 0);
                         printf("Writing encrypted file to SD card...\r\n");
                         /* Create output file */
                         write_to_file(fileNameOut, inputBuf, fileSizeRead);
                         printf("Done!\r\n");
                         break;
                     case '2': /* Decrypt */
-                        readSW0 = XGpio_DiscreteRead(&GpioSW0_Ptr, 1);
-                        if (readSW0) {
-                            printf("Starting ECB decrypt VERBOSE...\r\n");
-                        } else {
-                            printf("Starting ECB decrypt...\r\n");
-                        }
+                        printf("Starting ECB decrypt...\r\n");
                         printf("> Name decrypted file output\r\n");
                         prompt_file_input(fileNameOut);
                         AES_init_ctx(&ctx, key);
@@ -388,7 +337,7 @@ int main(void)
 						}
 #endif // MILESTONE3
 
-                        AES_ECB_decrypt_buffer(&ctx, inputBuf, fileSizeRead, readSW0);
+                        AES_ECB_decrypt_buffer(&ctx, inputBuf, fileSizeRead, 0);
                         printf("Writing decrypted file to SD card...\r\n");
                         /* Create output file */
                         write_to_file(fileNameOut, inputBuf, fileSizeRead);
