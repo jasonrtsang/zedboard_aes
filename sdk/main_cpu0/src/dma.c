9b024d174f81cc780aaa40786f31ca09653d6475
Jason Tsang <jasonrtsang@gmail.com>
2018-05-03 11:46:57 -0700

    Clean sdk files

--- sdk/main_cpu0/src/dma.c
+++ sdk/main_cpu0/src/dma.c
@@ -8,7 +8,7 @@
 #include "common.h"
 
 /******************************* Definitions *********************************/
-#define DMA_DEV_ID		       XPAR_AXIDMA_0_DEVICE_ID
+#define DMA_DEV_ID             XPAR_AXIDMA_0_DEVICE_ID
 
 #define MAX_PKT_LEN_WORDS_SEND 4
 #define MAX_PKT_LEN_SEND       MAX_PKT_LEN_WORDS_SEND*4
@@ -32,36 +32,36 @@
 /*****************************************************************************/
 bool dma_init(XAxiDma *axiDma)
 {
-	XAxiDma_Config *CfgPtr;
-	int Status;
+    XAxiDma_Config *CfgPtr;
+    int Status;
 
-	// Initialize the XAxiDma device
-	CfgPtr = XAxiDma_LookupConfig(DMA_DEV_ID);
-	if(!CfgPtr) {
+    // Initialize the XAxiDma device
+    CfgPtr = XAxiDma_LookupConfig(DMA_DEV_ID);
+    if(!CfgPtr) {
 #if UART_PRINT
-		printf("No config found for %d\r\n", DMA_DEV_ID);
+        printf("No config found for %d\r\n", DMA_DEV_ID);
 #endif
-		return false;
-	}
-	Status = XAxiDma_CfgInitialize(axiDma, CfgPtr);
-	if(Status != XST_SUCCESS) {
+        return false;
+    }
+    Status = XAxiDma_CfgInitialize(axiDma, CfgPtr);
+    if(Status != XST_SUCCESS) {
 #if UART_PRINT
-		xil_printf("Initialization failed %d\r\n", Status);
+        xil_printf("Initialization failed %d\r\n", Status);
 #endif
-		return false;
-	}
-	if(XAxiDma_HasSg(axiDma)){
+        return false;
+    }
+    if(XAxiDma_HasSg(axiDma)){
 #if UART_PRINT
-		xil_printf("Device configured as SG mode \r\n");
+        xil_printf("Device configured as SG mode \r\n");
 #endif
-		return false;
-	}
+        return false;
+    }
 
-	// Disable interrupts, using polling mode for DMA
-	XAxiDma_IntrDisable(axiDma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
-	XAxiDma_IntrDisable(axiDma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
+    // Disable interrupts, using polling mode for DMA
+    XAxiDma_IntrDisable(axiDma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
+    XAxiDma_IntrDisable(axiDma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
 
-	return true;
+    return true;
 }
 
 /*****************************************************************************/
@@ -70,8 +70,8 @@ bool dma_init(XAxiDma *axiDma)
 * Streaming cipher states to the IP block
 *
 * @param    XAxiDma* axiDma           : Pointer to AXI DMA
-* 			uint32_t *inputBuf        : Input state to stream to IP
-* 			uint32_t *outputBuf       : Output state from IP
+*           uint32_t *inputBuf        : Input state to stream to IP
+*           uint32_t *outputBuf       : Output state from IP
 *
 * @return   true if successful, false otherwise
 *
@@ -81,22 +81,22 @@ bool dma_init(XAxiDma *axiDma)
 /*****************************************************************************/
 bool dma_aes_process_transfer(XAxiDma* axiDma, uint32_t *inputBuf, uint32_t *outputBuf) {
 
-	// Flush the inputBuf before the DMA transfer, in case the Data Cache is enabled
-	Xil_DCacheFlushRange((u32)inputBuf, MAX_PKT_LEN_SEND);
+    // Flush the inputBuf before the DMA transfer, in case the Data Cache is enabled
+    Xil_DCacheFlushRange((u32)inputBuf, MAX_PKT_LEN_SEND);
 
-	if(XST_SUCCESS != XAxiDma_SimpleTransfer(axiDma,(u32) outputBuf, MAX_PKT_LEN_RCV, XAXIDMA_DEVICE_TO_DMA)) {
-		return false;
-	}
+    if(XST_SUCCESS != XAxiDma_SimpleTransfer(axiDma,(u32) outputBuf, MAX_PKT_LEN_RCV, XAXIDMA_DEVICE_TO_DMA)) {
+        return false;
+    }
 
-	if(XST_SUCCESS != XAxiDma_SimpleTransfer(axiDma,(u32) inputBuf, MAX_PKT_LEN_SEND, XAXIDMA_DMA_TO_DEVICE)) {
-		return false;
-	}
+    if(XST_SUCCESS != XAxiDma_SimpleTransfer(axiDma,(u32) inputBuf, MAX_PKT_LEN_SEND, XAXIDMA_DMA_TO_DEVICE)) {
+        return false;
+    }
 
-	while (XAxiDma_Busy(axiDma,XAXIDMA_DMA_TO_DEVICE));
-	while (XAxiDma_Busy(axiDma,XAXIDMA_DEVICE_TO_DMA));
+    while (XAxiDma_Busy(axiDma,XAXIDMA_DMA_TO_DEVICE));
+    while (XAxiDma_Busy(axiDma,XAXIDMA_DEVICE_TO_DMA));
 
-	// Invalidate outputBuf cache to receive data from DMA
-	Xil_DCacheInvalidateRange((u32)outputBuf, MAX_PKT_LEN_RCV);
+    // Invalidate outputBuf cache to receive data from DMA
+    Xil_DCacheInvalidateRange((u32)outputBuf, MAX_PKT_LEN_RCV);
 
-	return true;
+    return true;
 }
--- sdk/main_cpu0/src/oled.c
+++ sdk/main_cpu0/src/oled.c
@@ -32,17 +32,17 @@ void _oled_format_cursor(char inputLine[], char *outputLine) {
    int i = 1, j, length = 0;
 
    for(j = 0; j < 16; j++) {
-	   outputLine[j] = '\0';
+       outputLine[j] = '\0';
    }
    outputLine[0] = '*';
 
    while(inputLine[length] != '\0') {
-	   length++;
+       length++;
    }
 
    while(i < 15 && i < length) {
-	   outputLine[i] = inputLine[i];
-	   i++;
+       outputLine[i] = inputLine[i];
+       i++;
    }
 }
 
@@ -67,36 +67,36 @@ void _oled_format_cursor(char inputLine[], char *outputLine) {
 **/
 /*****************************************************************************/
 void _oled_refresh_oled(char* printLines[], int numOfLines, int index, bool menu) {
-	char formattedLine[16];
+    char formattedLine[16];
 
-	clear();
-	if(menu) {
-		print_message(printLines[0], 0);
-		if(index-1 >= 1 && index-1 < numOfLines) {
-			print_message(printLines[index-1], 1);
-		}
-		if(index < numOfLines) {
-			_oled_format_cursor(printLines[index], formattedLine);
-			print_message(formattedLine, 2);
-		}
-		if(index+1 < numOfLines) {
-			print_message(printLines[index+1], 3);
-		}
-	} else {
-		if(index < numOfLines) {
-			print_message(printLines[index], 0);
-		}
-		if(index+1 < numOfLines) {
-			print_message(printLines[index+1], 1);
-		}
-		if(index+2 < numOfLines) {
-			print_message(printLines[index+2], 2);
-		}
-		if(index+3 < numOfLines) {
-			print_message(printLines[index+3], 3);
-		}
+    clear();
+    if(menu) {
+        print_message(printLines[0], 0);
+        if(index-1 >= 1 && index-1 < numOfLines) {
+            print_message(printLines[index-1], 1);
+        }
+        if(index < numOfLines) {
+            _oled_format_cursor(printLines[index], formattedLine);
+            print_message(formattedLine, 2);
+        }
+        if(index+1 < numOfLines) {
+            print_message(printLines[index+1], 3);
+        }
+    } else {
+        if(index < numOfLines) {
+            print_message(printLines[index], 0);
+        }
+        if(index+1 < numOfLines) {
+            print_message(printLines[index+1], 1);
+        }
+        if(index+2 < numOfLines) {
+            print_message(printLines[index+2], 2);
+        }
+        if(index+3 < numOfLines) {
+            print_message(printLines[index+3], 3);
+        }
 
-	}
+    }
 }
 
 /*****************************************************************************/
@@ -113,12 +113,12 @@ void _oled_refresh_oled(char* printLines[], int numOfLines, int index, bool menu
 **/
 /*****************************************************************************/
 void oled_init(void) {
-	if (XST_SUCCESS != XGpio_Initialize(&gpioDpad, XPAR_BTN_GPIO_AXI_DEVICE_ID)) {
+    if (XST_SUCCESS != XGpio_Initialize(&gpioDpad, XPAR_BTN_GPIO_AXI_DEVICE_ID)) {
 #if UART_PRINT
-		printf("UH OH: BTN5 GPIO initialization failed\r\n");
+        printf("UH OH: BTN5 GPIO initialization failed\r\n");
 #endif
-	}
-	XGpio_SetDataDirection(&gpioDpad, 1, 1);
+    }
+    XGpio_SetDataDirection(&gpioDpad, 1, 1);
 }
 
 /*****************************************************************************/
@@ -135,7 +135,7 @@ void oled_init(void) {
 **/
 /*****************************************************************************/
 void oled_clear(void) {
-	clear();
+    clear();
 }
 
 /*****************************************************************************/
@@ -148,12 +148,12 @@ void oled_clear(void) {
 * @return   None
 *
 * @note     Expects each line to be 16 char aligned
-* 			Expects 4 lines in input
+*           Expects 4 lines in input
 *
 **/
 /*****************************************************************************/
 void oled_print_screen(char* printLines[]) {
-	_oled_refresh_oled(printLines, 4, 0, false);
+    _oled_refresh_oled(printLines, 4, 0, false);
 }
 
 /*****************************************************************************/
@@ -167,23 +167,23 @@ void oled_print_screen(char* printLines[]) {
 * @return   None
 *
 * @note     Expects each line to be 16 char aligned
-* 			Expects 4 lines in input
+*           Expects 4 lines in input
 *
 **/
 /*****************************************************************************/
 bool oled_confirmation_screen(char* printLines[]) {
-	enum DPAD dpadClick;
+    enum DPAD dpadClick;
 
-	oled_print_screen(printLines);
+    oled_print_screen(printLines);
 
-	while(1) {
-		dpadClick = XGpio_DiscreteRead(&gpioDpad, 1);
-		if(dpadClick == CENTER) {
-			return true;
-		} else if(dpadClick == LEFT) {
-			return false;
-		}
-	}
+    while(1) {
+        dpadClick = XGpio_DiscreteRead(&gpioDpad, 1);
+        if(dpadClick == CENTER) {
+            return true;
+        } else if(dpadClick == LEFT) {
+            return false;
+        }
+    }
 }
 
 /*****************************************************************************/
@@ -203,41 +203,41 @@ bool oled_confirmation_screen(char* printLines[]) {
 **/
 /*****************************************************************************/
 int oled_selection_screen(char* menuLines[], int numOfLines) {
-	int i = 1; // 1st line is menu title
-	bool exitMenu = false;
-	enum DPAD dpadClick;
+    int i = 1; // 1st line is menu title
+    bool exitMenu = false;
+    enum DPAD dpadClick;
 
-	_oled_refresh_oled(menuLines, numOfLines, i, true);
+    _oled_refresh_oled(menuLines, numOfLines, i, true);
 
-	while(!exitMenu)
-	{
-		dpadClick = XGpio_DiscreteRead(&gpioDpad, 1);
-		switch(dpadClick) {
-			case CENTER:
-				exitMenu = true;
-				break;
-			case DOWN:
-				clear();
-				if(i >= 1 && i < numOfLines-1) {
-					i+=1;
-				}
-				_oled_refresh_oled(menuLines, numOfLines, i, true);
-				break;
-			case UP:
-				clear();
-				if(i > 1 && i < numOfLines) {
-					i-=1;
-				}
-				_oled_refresh_oled(menuLines, numOfLines, i, true);
-				break;
-			case LEFT:
-				return 0;
-			default:
-				break;
-		}
-	}
+    while(!exitMenu)
+    {
+        dpadClick = XGpio_DiscreteRead(&gpioDpad, 1);
+        switch(dpadClick) {
+            case CENTER:
+                exitMenu = true;
+                break;
+            case DOWN:
+                clear();
+                if(i >= 1 && i < numOfLines-1) {
+                    i+=1;
+                }
+                _oled_refresh_oled(menuLines, numOfLines, i, true);
+                break;
+            case UP:
+                clear();
+                if(i > 1 && i < numOfLines) {
+                    i-=1;
+                }
+                _oled_refresh_oled(menuLines, numOfLines, i, true);
+                break;
+            case LEFT:
+                return 0;
+            default:
+                break;
+        }
+    }
 
-	return i;
+    return i;
 }
 
 /*****************************************************************************/
@@ -264,10 +264,10 @@ char** oled_format_fileList(char* inputFiles[], int numOfFiles) {
    strcpy(outputFiles[0], fileList_title);
 
    for(i = 0; i < numOfFiles; i++) {
-	   outputFiles[i+1] = malloc(sizeof(inputFiles[i]) * sizeof(char));
-	   strcpy(outputFiles[i+1], " ");
-	   strcpy(outputFiles[i+1]+1, " ");
-	   strcpy(outputFiles[i+1]+2, inputFiles[i]);
+       outputFiles[i+1] = malloc(sizeof(inputFiles[i]) * sizeof(char));
+       strcpy(outputFiles[i+1], " ");
+       strcpy(outputFiles[i+1]+1, " ");
+       strcpy(outputFiles[i+1]+2, inputFiles[i]);
    }
 
   return outputFiles;
@@ -287,5 +287,5 @@ char** oled_format_fileList(char* inputFiles[], int numOfFiles) {
 **/
 /*****************************************************************************/
 void oled_print_line(char* printLine, int index) {
-	print_message(printLine, index);
+    print_message(printLine, index);
 }
